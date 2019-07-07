4c08510b33351fae569d13f2242f086a26811917
dcdev-common <dcdev_founder@qq.com>
2019-07-07 08:57:41 +0800

    添加debug函数

--- dcConsole.js
+++ dcConsole.js
@@ -34,3 +34,19 @@ function showConsole() {
     document.getElementById("console").style.width = "80px";
     document.getElementById("console").style.height = "28px";
 }
+
+// 输出调试代码函数
+function debug(o) {
+    var gettype = Object.prototype.toString
+        // 是一个对象
+    if (gettype.call(o) == "[object Object]") {
+        try {
+            o = JSON.stringify(o);
+            o = o.replace("{", "{<br>&nbsp;&nbsp;&nbsp;&nbsp;");
+            o = o.replace("}", "<br>}");
+            o = o.replace("\",", "\",<br>&nbsp;&nbsp;&nbsp;&nbsp;");
+        } catch (e) {}
+    }
+    var time = new Date();
+    sessionStorage.log = time.toLocaleString() + '<br>' + o + '<hr>' + sessionStorage.log;
+}
